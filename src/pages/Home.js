cbdd5857aa85493c88054a88311e7a749df7236c
Lucas Barbosa <contato@lucasbarbosa.dev>
2022-09-25 00:19:35 -0300

    :sparkles: My Skills

---
+++ src/pages/Home.js
@@ -0,0 +1,87 @@
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  FlatList,
+} from 'react-native';
+import {Button} from '../components/Button';
+import {SkillCard} from '../components/SkillCard';
+
+export function Home() {
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState([]);
+  const [greeting, setGreeting] = useState('');
+
+  function handleAddNewSkill() {
+    if (newSkill !== '') {
+      setMySkills(oldState => [...oldState, newSkill]);
+      setNewSkill('');
+    }
+  }
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour < 12) {
+      setGreeting('Good morning');
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGreeting('Good afternoon');
+    } else {
+      setGreeting('Good night');
+    }
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Lucas!</Text>
+
+      <Text style={styles.greetings}>{greeting}</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="New Skill"
+        placeholderTextColor="#555"
+        value={newSkill}
+        onChangeText={setNewSkill}
+      />
+
+      <Button onPress={handleAddNewSkill} />
+
+      <Text style={[styles.title, {marginVertical: 30}]}>My Skills</Text>
+
+      <FlatList
+        data={mySkills}
+        keyExtractor={(item, index) => index}
+        renderItem={({item}) => <SkillCard skill={item} />}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  greetings: {
+    color: '#fff',
+  },
+});
